b98872c056c3a41e1a66a8424a3fb068b3f819f2
Sidhant <alairoet@gmail.com>
2023-06-15 16:43:59 +0530

    fix: :ambulance: middleware on get-user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const AuthController = require('../controllers/auth.controller');
+const authMiddleware = require("../middlewares/auth.middleware")
 
 router.post('/login', AuthController.login);
-router.post('/get-user',AuthController.getUser)
+router.post('/get-user', authMiddleware,AuthController.getUser)
 
 
 module.exports = router;
